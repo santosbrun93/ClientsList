a4d1d541792f0125fbf9a08f7dc8af9fa323271c
santosbrun93 <41234430+santosbrun93@users.noreply.github.com>
2019-10-15 19:39:20 -0300

    Add files via upload

---
+++ ClientsList/src/main/java/Controller/HibernateUtil.java
@@ -0,0 +1,52 @@
+package controller;
+
+import model.*;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory = null;
+
+    static {
+        try{
+            loadSessionFactory();
+        }catch(Exception e){
+            System.err.println("Exception while initializing hibernate util // Exceção durante inicialização do hibernate util ");
+        }
+    }
+
+    public static void loadSessionFactory(){
+
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        configuration.addAnnotatedClass(Pessoa.class);
+        configuration.addAnnotatedClass(Cliente.class);
+        configuration.addAnnotatedClass(Telefone.class);
+        configuration.addAnnotatedClass(Endereco.class);
+
+
+        ServiceRegistry srvcReg = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(srvcReg);
+    }
+
+    public static Session getSession() throws HibernateException {
+
+        Session retSession=null;
+        try {
+            retSession = sessionFactory.openSession();
+        }catch(HibernateException t){
+            System.err.println("Exception while getting session  // Exceção enquanto buscando sessão ");
+        }
+        if(retSession == null) {
+            System.err.println("session is discovered null  //  Sessão descoberta está nula ");
+        }
+
+        return retSession;
+    }
+}
+
---
+++ ClientsList/src/main/java/Model/Cliente.java
@@ -0,0 +1,34 @@
+package Model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CLIENTES")
+
+public class Cliente extends Pessoa implements Serializable {
+
+    
+    private long idCliente;
+    
+    
+    public Cliente() {
+    }
+
+    public Cliente(long idCliente, String nome, Date nascimento, String cpf, long rg, Collection<Endereco> enderecos, Collection<Telefone> telefones) {
+        super(nome, nascimento, cpf, rg, enderecos, telefones);
+        this.idCliente = idCliente;
+    } 
+
+    
+    public long getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(long idCliente) {
+        this.idCliente = idCliente;
+    }
+}
